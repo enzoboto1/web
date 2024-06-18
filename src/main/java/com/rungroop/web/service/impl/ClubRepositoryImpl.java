32bbdc5ddeda7d120fcaa323e3e072d6e5b5fa8d
enzoboto1 <enzoboto1.0@gmail.com>
2024-06-18 11:36:42 -0300

    Create service.impl ClubServiceImpl

---
+++ src/main/java/com/rungroop/web/service/impl/ClubRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.rungroop.web.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rungroop.web.dto.ClubDto;
+import com.rungroop.web.models.Club;
+import com.rungroop.web.repository.ClubRepository;
+import com.rungroop.web.service.ClubService;
+
+public class ClubRepositoryImpl implements ClubService{
+
+    @Autowired
+    private ClubRepository clubRepository;
+
+    @Override
+    public List<ClubDto> findAllClubs() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map((club) -> mapToClubDto(club)).collect(Collectors.toList());
+    }
+
+    private ClubDto mapToClubDto(Club club) {
+        return ClubDto.builder()
+                        .id(club.getId())
+                        .title(club.getTitle())
+                        .photoUrl(club.getPhotoUrl())
+                        .content(club.getContent())
+                        .createOn(club.getCreateOn())
+                        .updateOn(club.getUpdateOn())
+                        .build();
+    }
+    
+}
