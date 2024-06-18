eb604e823116a55329de88a2b0ecc0c9a0bd69f4
enzoboto1 <enzoboto1.0@gmail.com>
2024-06-18 10:59:25 -0300

    Create dto ClubDto

---
+++ src/main/java/com/rungroop/web/dto/ClubDto.java
@@ -0,0 +1,18 @@
+package com.rungroop.web.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ClubDto {
+    
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    private LocalDateTime createOn;
+    private LocalDateTime updateOn;
+}
