c265ab8b2d34a715cfacb393e59cb3af8fdf5d19
enzoboto1 <enzoboto1.0@gmail.com>
2024-06-20 23:59:40 -0300

    Method Create Club

--- src/main/java/com/rungroop/web/controller/ClubController.java
+++ src/main/java/com/rungroop/web/controller/ClubController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import com.rungroop.web.dto.ClubDto;
+import com.rungroop.web.models.Club;
 import com.rungroop.web.service.ClubService;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -24,6 +25,13 @@ public class ClubController {
         List<ClubDto> clubs = clubService.findAllClubs();
         model.addAttribute("clubs", clubs);
         return "clubs-list";
+    }
+
+    @GetMapping("/clubs/new")
+    public String createClubForm(Model model) {
+        Club club = new Club();
+        model.addAttribute("club", club);
+        return "clubs-create";
     }
     
 }
