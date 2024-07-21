a0c8a46112b506c2e5f8ed0d2307fc9e08ef84a9
enzoboto1 <enzoboto1.0@gmail.com>
2024-07-20 21:37:54 -0300

    Ajuste UserRepository

--- src/main/java/com/rungroop/web/repository/UserRepository.java
+++ src/main/java/com/rungroop/web/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.rungroop.web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rungroop.web.dto.RegistrationDto;
 import com.rungroop.web.models.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    public RegistrationDto findByUsername(String username);
-    public RegistrationDto findByEmail(String email);
+    UserEntity findByEmail(String email);
+    UserEntity findByUsername(String userName);
 }
