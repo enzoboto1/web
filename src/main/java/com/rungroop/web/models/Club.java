a9fc386fe51482dae54bd0d5ba02534f09bc3478
enzoboto1 <enzoboto1.0@gmail.com>
2024-07-22 21:05:23 -0300

    Novo campo createdBy em Club

--- src/main/java/com/rungroop/web/models/Club.java
+++ src/main/java/com/rungroop/web/models/Club.java
@@ -12,6 +12,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -37,7 +39,12 @@ public class Club {
     @UpdateTimestamp
     private LocalDateTime updatedOn;
 
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private UserEntity createdBy;
+
     @Builder.Default
     @OneToMany(mappedBy = "club", cascade = CascadeType.REMOVE)
     private List<Event> events = new ArrayList<>();
+    
 }
