9e0f5a62c20be19b153273a5dfe84daf0c727c8c
enzoboto1 <enzoboto1.0@gmail.com>
2024-07-22 21:06:23 -0300

    Adicionando metodo findFirstByUsername em UserRepository

--- src/main/java/com/rungroop/web/repository/UserRepository.java
+++ src/main/java/com/rungroop/web/repository/UserRepository.java
@@ -6,4 +6,5 @@ import com.rungroop.web.models.UserEntity;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByUsername(String userName);
+    UserEntity findFirstByUsername(String userName);
 }
